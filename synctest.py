efb790b4a8020a7cefcb8e51d06ebbdfe0279ba8
me <nmapsy@gmail.com>
2013-08-27 11:26:22 -0400

    Version 0.8

--- synctest.py
+++ synctest.py
@@ -2,19 +2,10 @@
 
 import os
 import sys
+import random
+
 
 def main():
-  test_matchup()
-  sys.exit(0)
-  arr1 = ['a', 'c', 'd']
-  arr2 = ['a', 'b', 'c', 'd']
-  (missing1, missing2) = matchup(arr1, arr2)
-  print "arr1: "+str(arr1)
-  print "arr2: "+str(arr2)
-  print "missing1: "+str(missing1)
-  print "missing2: "+str(missing2)
-  sys.exit(0)
-
   rootdir1 = ''
   rootdir2 = ''
   if len(sys.argv) > 2:
@@ -110,33 +101,77 @@ def matchup(files1, files2):
       found_it = False;
       # find the first file on either side that matches, load up the dict's
       while not found_it and not (a >= len1 and b >= len2):
-        print "\t"+str(a)+":"+files1[a]+" "+str(b)+":"+files2[b]
-        skipped1[files1[a]] = a
-        skipped2[files2[b]] = b
-        if files1[a] in skipped2:
-          found_it = True
-          print "found "+files1[a]+" in arr2, index "+str(a)+" in arr1"
-          i_end = a
-          j_end = skipped2[files1[a]]
-        elif a < len1 - 1:
-          a+=1
-        if files2[b] in skipped1:
-          found_it = True
-          print "found "+files2[b]+" in arr1, index "+str(b)+" in arr2"
-          j_end = b
-          i_end = skipped1[files2[b]]
-        elif b < len2 - 1:
-          b+=1
+        sys.stdout.write("\t"+str(a)+":")
+        if a < len1:
+          sys.stdout.write(files1[a])
+        else:
+          sys.stdout.write("#")
+        sys.stdout.write(" "+str(b)+":")
+        if b < len2:
+          sys.stdout.write(files2[b])
+        else:
+          sys.stdout.write("#")
+        print ""
+
+        if a < len1:
+          skipped1[files1[a]] = a
+          if files1[a] in skipped2:
+            found_it = True
+            print "found "+files1[a]+" in arr2, index "+str(a)+" in arr1"
+            i_end = a
+            j_end = skipped2[files1[a]]
+          elif a < len1:
+            # i_end = a   # I thought something like this would help
+            a+=1
+
+        if b < len2:
+          skipped2[files2[b]] = b
+          if files2[b] in skipped1:
+            found_it = True
+            print "found "+files2[b]+" in arr1, index "+str(b)+" in arr2"
+            j_end = b
+            i_end = skipped1[files2[b]]
+          elif b < len2:
+            # j_end = b
+            b+=1
+
+        # I changed the exit condition from a >= len1-1 and b >= len2-1
+        #                                to a >= len1   and b >= len2
+        # and that made it work for all the previously working tests
+        # but not the mismatch-tail ones
+        if not found_it and a >= len1 and b >= len2:
+          print "entering mismatch-tail mode"
+          i_end = len1
+          j_end = len2
+
+        sys.stdout.write(str(a)+" >= "+str(len1)+" and "+str(b)+" >= "
+          +str(len2)+" ?? ")
+        if a >= len1 and b >= len2:
+          print "   ..Yes."
+        else:
+          print "   ..No."
+        #print str(not found_it and not (a >= len1 - 1 and b >= len2 - 1))
+
+      # So it's getting out here. Now the task is to have it recognize that
+      # situation and deal with it:
+      # The situation where they're at the end of both arrays AND there's a
+      # mismatch. Actually, you'll probably want to record it in the inner loop.
+      print "exited inner loop"
+      print "i = "+str(i)+", i_end = "+str(i_end)
+      print "j = "+str(j)+", j_end = "+str(j_end)
+
       # TODO handle case where the matches aren't found.
 
       # then start again at i and j, and add each file to missing until you
       # hit the first one that's present in the other's skipped
       i_tmp = i
       j_tmp = j
-      print ("deleting everything from ["+str(i_tmp)+" to "
-        +str(i_end-1)+"] from arr1")
-      print ("                     and ["+str(j_tmp)+" to "
-        +str(j_end-1)+"] from arr2")
+      if i_tmp < i_end:
+        print ("deleting everything from ["+str(i_tmp)+" to "
+          +str(i_end-1)+"] from arr1")
+      if j_tmp < j_end:
+        print ("                     and ["+str(j_tmp)+" to "
+          +str(j_end-1)+"] from arr2")
       while i_tmp < i_end:
         print "adding "+str(i)+":"+files1[i]+" from files1 to missing1"
         missing1.append(files1[i])
@@ -204,6 +239,10 @@ def test_matchup():
       ['d', 'e', 'f'],          ],
     [ ['a', 'b', 'c', 'd'],       #13
       ['a', 'e', 'f'],          ],
+    [ ['a', 'b', 'e', 'f'],       #14
+      ['c', 'd', 'e', 'f'],     ],
+    [ ['a', 'b', 'd', 'e'],       #15
+      ['c', 'd', 'f', 'g'],     ],
   ]
   outputs = [
     # 0: output arr1   1: missing1
@@ -235,19 +274,37 @@ def test_matchup():
     [ [], ['a', 'b', 'c'],        #12
       [], ['d', 'e', 'f'],      ],
     [ ['a'], ['b', 'c', 'd'],     #13
-      ['a'], ['d', 'e'],        ],
+      ['a'], ['e', 'f'],        ],
+    [ ['e', 'f'], ['a', 'b'],     #14
+      ['e', 'f'], ['c', 'd'],   ],
+    [ ['d'], ['a', 'b', 'e'],     #15
+      ['d'], ['c', 'f', 'g'],   ],
   ]
+
+  # a second block, for readability
+  inputs.extend([
+    [ ['a'],                      #16
+      ['a'],                    ],
+    [ ['a'],                      #17
+      ['b'],                    ],
+      # include one (or two) with empty inputs!
+  ])
+  outputs.extend([
+    [ ['a'], [],                  #16
+      ['a'], [],                ],
+    [ [], ['a'],                  #17
+      [], ['b'],                ],
+  ])
 
   for inout_pair in zip(inputs, outputs):
     inputt = inout_pair[0]
     output = inout_pair[1]
+    print "============================================================"
     print "starting: "+str(inputt[0])
     print "      and "+str(inputt[1])
     (missing1, missing2) = matchup(inputt[0], inputt[1])
-    print "missing1: "+str(missing1)
-    print "missing2: "+str(missing2)
-    print "modified arr1: "+str(inputt[0])
-    print "modified arr2: "+str(inputt[1])
+    print "modified arr: "+str(inputt[0])+"\t\t"+str(inputt[1])
+    print "missing elem: "+str(missing1)+"\t\t"+str(missing2)
     if (missing1 == output[1] and missing2 == output[3] and
       inputt[0] == output[0] and inputt[1] == output[2]):
       print "output match"
@@ -265,9 +322,95 @@ def test_matchup():
     print ""
 
   if failures:
-    print "did not pass! "+str(failures)+" failures."
+    print "Did not pass! "+str(failures)+" failures."
   else:
-    print "passed all tests!"
+    print "Passed all tests!"
+
+
+def fuzz_test():
+  passed = True
+  LETTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+    'q','r','s','t','u','v','w','x','y','z']
+  ARR_MAX = 9
+
+  letters_left = list(LETTERS)
+
+  arr1 = []
+  startlength = random.randint(0, ARR_MAX)
+  for i in range(startlength):
+    l = random.randint(0, len(letters_left)-1)
+    arr1.append(letters_left[l])
+    del(letters_left[l])
+
+  arr1.sort()
+  arr2 = list(arr1)
+
+  missing2 = []
+  # up to one less than the length of the original array
+  # (leave at minimum one element left)
+  if len(arr1) > 0:
+    for i in range(random.randint(0, len(arr1)-1)):
+      l = random.randint(0, len(arr1)-1)
+      missing2.append(arr1[l])
+      del(arr1[l])
+  missing2.sort()
+
+  missing1 = []
+  if len(arr2) > 0:
+    for i in range(random.randint(0, len(arr2)-1)):
+      l = random.randint(0, len(arr2)-1)
+      if arr2[l] not in missing2: # don't delete the same item from both
+        missing1.append(arr2[l])
+        del(arr2[l])
+  missing1.sort()
+
+  end1 = []
+  for elem in arr1:
+    if elem not in missing1:
+      end1.append(elem)
+
+  end2 = []
+  for elem in arr2:
+    if elem not in missing2:
+      end2.append(elem)
+
+  # print "start1:   "+str(arr1)
+  # print "start2:   "+str(arr2)
+  # print "end1:     "+str(end1)
+  # print "end2:     "+str(end2)
+  # print "missing1: "+str(missing1)
+  # print "missing2: "+str(missing2)
+
+  (missing1real, missing2real) = matchup(arr1, arr2)
+  if (missing1 == missing1real and missing2 == missing2real
+    and end1 == arr1 and end2 == arr2):
+    print "output match"
+  else:
+    passed = False
+    print "No output match! Actual/Expected:"
+    print "missing1: "+str(missing1real)
+    print "expected: "+str(missing1)
+    print "missing2: "+str(missing2real)
+    print "expected: "+str(missing2)
+    print "new arr1: "+str(arr1)
+    print "expected: "+str(end1)
+    print "new arr2: "+str(arr2)
+    print "expected: "+str(end2)
+
+  return passed
+
+def fuzz_loop():
+  TESTS = 500
+
+  failures = 0
+  i = 0
+  while i < TESTS:
+    if not fuzz_test():
+      failures+=1
+    i+=1
+
+  print "\nfailures: "+str(failures)+"/"+str(TESTS)
+
 
 if __name__ == "__main__":
-  main()
+  fuzz_loop()
